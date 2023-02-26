c19d310ff75487dbf94f8f51a1fac72b17dc1f5b
feyz <faizashrafkk@gmail.com>
2023-02-26 13:24:17 +0530

    dash backend connection ready

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
@@ -32,4 +33,31 @@ const signupUser = async (req, res) => {
   }
 };
 
-module.exports = { loginUser, signupUser };
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(400)
+      .json({ error: "Id is not valid Please check again" });
+  }
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: id },
+      {
+        ...req.body,
+      }
+    );
+
+    if (!user) {
+      return res.status(400).json({ error: "User Not Found" });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { loginUser, signupUser, updateUser };
--- cms-project-web/src/pages/dashPages/Dash.js
+++ cms-project-web/src/pages/dashPages/Dash.js
@@ -1,17 +1,34 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../../Hook/useAuthContext";
+import { Link } from "react-router-dom";
 
 const Dash = () => {
   var today = new Date(),
     date = today.toTimeString();
 
-  const [college] = useState({
-    college: "Lissah College",
-    place: "Kaithapoyil",
-    teachersCount: 57,
-    StudentsCount: 650,
-    staffCount: 8,
-  });
+  const { user } = useAuthContext();
+  const [college, setCollege] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("api/college", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const json = await response.json();
+
+      if (response.ok) {
+        setCollege(json);
+        console.log(json);
+        setIsPending(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="dashHomee">
@@ -21,12 +38,46 @@ const Dash = () => {
           <p>{date}</p>
         </div>
         <div className="dHomeNavRight">
-          <h2>
-            {college.college}, {college.place}
-          </h2>
+          <h3>
+            {college && college[0].college_name}, {college && college[0].place}
+          </h3>
           <span className="material-symbols-outlined">notifications</span>
         </div>
       </div>
+
+      {isPending && <div className="loading">Loading....</div>}
+      {college && (
+        <div className="dashCollegeDetail">
+          <div className="dashCollegeDetailLeft">
+            <div className="countList">
+              <div className="teacherCount countListCounter">
+                <h1>{college[0].teachers_count}</h1>
+                <h6>Teacher</h6>
+              </div>
+              <div className="studentCount countListCounter">
+                <h1>{college[0].students_count}</h1>
+                <h6>Students</h6>
+              </div>
+              <div className="staffCount countListCounter">
+                <h1>{college[0].staff_count}</h1>
+                <h6>Non-Teaching Staff</h6>
+              </div>
+            </div>
+
+            <div className="dashTodos">
+              <div className="dashTodosNav">
+                <h3>Todos</h3>
+                <Link to="/">View All</Link>
+              </div>
+              <p>{">"} Todos are empty</p>
+            </div>
+          </div>
+          <div className="dashCollegeDetailRight">
+            <h3>Announcements</h3>
+            <p>{">"} No New Announcements</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
